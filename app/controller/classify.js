131a0f3300f56c19a5233a66f278db6ce05bca89
zp <1642907554@qq.com>
2020-12-15 09:38:06 +0800

    添加文章分类

---
+++ app/controller/classify.js
@@ -0,0 +1,9 @@
+const baseController = require('./baseController');
+
+class classifyController extends baseController{
+    async index(){
+        
+    }
+}
+
+module.exports=classifyController;
--- app/service/article.js
+++ app/service/article.js
@@ -13,17 +13,20 @@ class ArticleService extends Service {
                     offset: offsetData, // 数据偏移量
                 }
                 const row2={
-                    where: {userId:ctx.request.body.userInfo}, // WHERE 条件
+                    userId:ctx.request.body.userInfo, // WHERE 条件
                 }
                 //查询总条数
                 const result1= await this.app.mysql.count('blogName', row2);
-                console.log(result1)
+                
                 //根据分页查询结果
                 const resultPage = await this.app.mysql.select('blogName',row); 
                
-                return {msg:resultPage,total:result1
-                
+                return {
+                    msg:resultPage,
+                    total:result1,
+                    totalPage:Math.ceil(result1/ctx.request.body.pageSize*1)
                 };
+            
             }, ctx);
             return {
                 status:200,
